5e2a1a19dce532b6bd2b3c27957ddf1014042415
wanggeng <18367319791@163.com>
2022-10-14 16:09:43 +0800

    feat:增加ts类型体操练习

--- js/3-ts函数.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// 函数声明
-function add(x, y) {
-    return x + y;
-}
-// 函数表达式
-let myAdd = function (x, y) { return x + y; };
-// 接口
